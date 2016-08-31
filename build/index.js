f9cf8d829dd3d52fa910b5c619c7f61ac9384dcc
Kahlil Lechelt <hello@kahlil.info>
2016-08-31 16:11:58 +0200

    remove build folder and add it to gitignore

--- build/index.js
+++
@@ -1,6 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const TinyDraft = require('./app');
-
-ReactDOM.render(React.createElement(TinyDraft, null), document.getElementById('root'));
-//# sourceMappingURL=index.js.map
